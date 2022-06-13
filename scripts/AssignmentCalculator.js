f8060f47202f4bfbc16b494b2455fb6c479ce4fc
libredev-rayan <rian.h04@gmail.com>
2022-06-14 00:47:24 +0300

    updates-2

--- scripts/AssignmentCalculator.js
+++ scripts/AssignmentCalculator.js
@@ -1,57 +1,52 @@
-function add(num1, num2){
-    return num1 + num2
+let aaa = ''
+let bbb = ''
 
+function add(num1, num2) {
+    return num1 + num2
 }
-function subtract(num1, num2){
+function subtract(num1, num2) {
     return num1 - num2
 }
-function multiply(num1, num2){
+function multiply(num1, num2) {
     return num1 * num2
 }
-function divide(num1, num2){
-    return num2 ===0 ? 'cannot divided by zero'
-    : num1/num2;
+function divide(num1, num2) {
+    return num2 === 0 ? 'cannot divided by zero' : num1 / num2
 }
 
-function operate(operator, num1, num2){   
-    num1 = document.getElementById("result").value;
-    num2 = document.getElementById("result").value = num2;
-    
-    if (operator == "+") {
-        return multiply(num1, num2)
+function operate(operator, num1, num2) {
+    operator = aaa
+    console.log(aaa)
+    num1 = parseInt(document.getElementById('result').value)
+    num2 = 1
+
+    if (operator === '+') {
+        return add(num1, num2)
     }
-    if (operator == "-") {
-        return multiply(num1, num2)
+    if (operator === '-') {
+        return subtract(num1, num2)
     }
-    if (operator == "%") {
-        return multiply(num1, num2)
-    }    
-    if (operator=="*"){
+    if (operator === '%') {
+        return divide(num1, num2)
+    }
+    if (operator === '*') {
         return multiply(num1, num2)
     }
 }
 function clearScreen() {
-    document.getElementById("result").value = "";
+    document.getElementById('result').value = ''
 }
 
-function display(value) {
-    if (value === "+" ){
-        operate(value, num1, num2) // can not work
-    }
-    if (value === "-") {
-        operate(value, num1, num2) // can not work
-    }
-    if (value === "%") {
-        operate(value, num1, num2) // can not work
-    }
-    if (value === "*") {
-        operate(value, num1, num2) // can not work
-    }
-    document.getElementById("result").value += value;
+function display(value, proces) {
+    proces ? (aaa = proces) : null
+    console.log(aaa, proces)
+    document.getElementById('result').value += value
 }
 
 
-
+function equals(value, proces) {
+    display.innerText = eval(display.innerText)
+}
 
 
 
